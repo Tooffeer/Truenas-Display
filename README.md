87de0779e50a53393ee212ddb85029ffcb9fe538
Tooffeer <138416859+Tooffeer@users.noreply.github.com>
2025-10-04 01:43:02 -0700

    More stuff
    Needs to auto update, I want to make it look nice rather than just some text on the screen.

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Truenas Display
 
+Need to get waveshare shit and run demo maybe too see if things work
+
+
+sudo apt-get update
+sudo apt-get install python3-pip
+sudo apt-get install python3-pil
+sudo apt-get install python3-numpy
+sudo pip3 install spidev
+git clone https://github.com/waveshare/e-Paper.git
+
+
 Right now just run truenas_display to see if it works, also replace the key stuff
+
+Need to do more shit, whatever
--- truenas_display.py
+++ truenas_display.py
@@ -1,6 +1,6 @@
 import time
 import requests
-from waveshare_epd import epd2in13_v4
+from waveshare_epd import epd2in13_V4
 from PIL import Image, ImageDraw, ImageFont
 
 # Api config
@@ -14,7 +14,7 @@ def get_pool_info():
 
 def main():
     # Initialize and clear display
-    epd = epd2in13_v4.EPD()
+    epd = epd2in13_V4.EPD()
     epd.init()
     epd.Clear()
 
@@ -23,18 +23,42 @@ def main():
     image = Image.new('1', (epd.height, epd.width), 255)
     draw = ImageDraw.Draw(image)
 
-    # Fonts (default bitmap — can be swapped for TTF later)
-    font_big = ImageFont.load_default()
-    font_small = ImageFont.load_default()
+    # Fonts
+    font_small = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 18)
 
-    # --- Pool Info ---
-    y = 55
+    # Pool Info
+    y = 0
+    pools = get_pool_info()
     for pool in pools:
         pool_name = pool["name"]
         status = pool["status"]
+
+        free = int(pool["free"])
+        used = int(pool["allocated"])
+        
+        gb = 1024 ** 3
+        free_gb = free / gb
+        used_gb = used / gb
+        
+        
+
+        # Print text
         draw.text((5, y), f"Pool: {pool_name}", font=font_small, fill=0)
         draw.text((120, y), f"{status}", font=font_small, fill=0)
+
         y += 15
+        draw.text((120, y), f"Free: {free_gb}", font=font_small, fill=0)
+        y += 15
+        draw.text((120, y), f"Used: {used_gb}", font=font_small, fill=0)
+
+        percentage = (used / (free + used)) * 100
+
+        y += 15
+        draw.text((120, y), f"{percentage:.1f}%", font=font_small, fill=0)
 
     # Send to e-ink display
     epd.display(epd.getbuffer(image))
+    epd.sleep() # Sleep
+
+if __name__ == "__main__":
+    main()
